e15252431b68e637eb820223a3795991b689c58e
Danilo Diniz <dgdevbr@gmail.com>
2026-03-23 00:13:28 -0300

    adicionando minutos restantes

--- tempo_de_jogo.c
+++ tempo_de_jogo.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
 int main() {
-    int tempoMinutos, tempoHoras;
+    int tempoMinutos, tempoHoras, minRestantes;
 
     printf("\nDigite quanto tempo de jogo você tem em minutos: ");
     scanf("%d%*c", &tempoMinutos);
     tempoHoras = tempoMinutos / 60;
-    printf("Tempo de jogo em horas = %d\n", tempoHoras);
+    minRestantes = tempoMinutos % 60;
+
+    printf("Tempo de jogo: %d horas e %d minutos\n", tempoHoras, minRestantes);
 
     return 0;
 }
