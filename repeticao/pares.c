4f6b988a84f6a316c0c7d3717e5f0acd0e68ec88
Danilo Diniz <dgdevbr@gmail.com>
2026-03-27 11:12:53 -0300

    print pares

---
+++ repeticao/pares.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Digite um numero: ");
+    scanf("%d%*c", &n);
+
+    for (int i = 1; i <= n; i++) {
+        if (i % 2 == 0) {
+            printf("%d\n", i);
+        }
+    }
+}
