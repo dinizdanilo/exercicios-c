0057e356969caacdf30d2d10e4b0e46743d3d398
Danilo Diniz <dgdevbr@gmail.com>
2026-03-30 21:33:07 -0300

    declarando variaveis dentro do laço, renomeando variaveis

--- repeticao/triangulodois.c
+++ repeticao/triangulodois.c
@@ -1,21 +1,20 @@
 #include <stdio.h>
 
 int main() {
-    int n;
-    printf("Digite o valor de N: ");
-    scanf("%d%*c", &n);
+    int altura;
+    printf("Digite a altura do triangulo: ");
+    scanf("%d%*c", &altura);
 
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j <= n  - i; j++) {
+    for (int i = 0; i < altura; i++) {
+        for (int j = 0; j < altura  - i; j++) {
             printf(" "); 
         }
-            for (int k = 1; k <= (2 * i - 1); k++) {
-                printf("*");
-            }
+        
+        for (int k = 1; k <= (2 * i - 1); k++) {
+            printf("*");
+        }
         printf("\n");
     }
-
-
 
     return 0;
 }
