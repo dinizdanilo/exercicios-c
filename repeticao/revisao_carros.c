74d08209f102a0d598b051fd21e4819b6671a38e
Danilo Diniz <dgdevbr@gmail.com>
2026-03-30 21:14:44 -0300

    declarando variaveis dentro do laço e tirando redundancia das condicionais

--- repeticao/revisao_carros.c
+++ repeticao/revisao_carros.c
@@ -2,30 +2,29 @@
 
 int main() {
     int qtdCarros;
-    char modelo[50];
     printf("Quantos carros serão registrados?: ");
     scanf("%d%*c", &qtdCarros);
-
+    
     for (int i = 0; i < qtdCarros; i++) {
+        int anoFabricacao, funcionando;
+        char modelo[50];
+
         printf("\nDigite o modelo do carro: ");
         scanf("%s", modelo);
 
-        int anoFabricacao;
         printf("Digite o ano de fabricação do carro: ");
         scanf("%d%*c", &anoFabricacao);
 
-        int funcionando;
         printf("Está funcionando normalmente? (1 | 0): ");
         scanf("%d%*c", &funcionando);
 
         if (anoFabricacao < 2005 && funcionando == 0) {
             printf("O carro %s precisa de REPAROS URGENTES!\n", modelo);
-        } else if (anoFabricacao < 2005 && funcionando == 1) {
+        } else if (anoFabricacao < 2005) {
             printf("O carro %s é antigo, recomenda-se uma revisão!\n", modelo);
-        } else if (anoFabricacao >= 2005 && funcionando == 0) {
+        } else if (funcionando == 0) {
             printf("O carro %s precisa de manutenção!\n", modelo);
-        }
-        else {
+        } else {
             printf("O carro %s está em boas condições!\n", modelo);
         }
     }
