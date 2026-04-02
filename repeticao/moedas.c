21d7ee859180ce7a0ea388de1bf079a20175e543
Danilo Diniz <dgdevbr@gmail.com>
2026-04-02 10:26:29 -0300

    exercicio com acumulador e contador em while

--- repeticao/moedas.c
+++ repeticao/moedas.c
@@ -5,19 +5,16 @@ int main() {
     int moedasObtidas;
     int contador = 0;
 
-    do {
+    while (moedas < 100) {
         printf("Digite a quantidade de moedas obtidas (1 a 10): ");
         scanf("%d%*c", &moedasObtidas);
-
-        if (moedasObtidas < 1 && moedasObtidas > 10) {
-            continue;
-        }
-
+    
         moedas += moedasObtidas;
         contador++;
-
-    } while (moedas != 100);
+    }
 
     printf("\nVocê obteve 100 moedas!\n");
     printf("A quantidade de rodadas necessárias foram: %d\n", contador);
+
+    return 0;
 }
