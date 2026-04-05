8e2ea069e41e6dd6ffdb90149f0370d7a3a5aa67
Danilo Diniz <dgdevbr@gmail.com>
2026-04-05 12:54:35 -0300

    maior de tres numeros

---
+++ funcoes/maior_numero.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int maior(int num1, int num2, int num3) {
+    if (num1 > num2 && num1 > num3) {
+        return num1;
+    } else if (num2 > num3) {
+        return num2;
+    } else {
+        return num3;
+    }
+}
+
+int main() {
+    int num1, num2, num3;
+
+    printf("Digite o primeiro numero: ");
+    scanf("%d%*c", &num1);
+    printf("Digite o segundo numero: ");
+    scanf("%d%*c", &num2);
+    printf("Digite o terceiro numero: ");
+    scanf("%d%*c", &num3);
+
+    printf("O maior numero que você digitou foi: %d\n", maior(num1, num2, num3));
+
+    return 0;
+}
