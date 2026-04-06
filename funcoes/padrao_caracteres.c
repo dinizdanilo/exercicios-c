8a2f491dc9267b3ce2e0320d1b2aaea474209445
Danilo Diniz <dgdevbr@gmail.com>
2026-04-06 17:01:31 -0300

    espaço no print dos caracteres

--- funcoes/padrao_caracteres.c
+++ funcoes/padrao_caracteres.c
@@ -7,6 +7,7 @@ void menu() {
 }
 
 void quadrado(int n, char caracter) {
+    printf("\n");
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             printf("%c ", caracter);
